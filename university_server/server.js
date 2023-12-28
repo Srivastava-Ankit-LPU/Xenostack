d6d8f613b31ee04065e7c9629ccb68c91696d76e
Srivastava-Ankit-LPU <83294365+Srivastava-Ankit-LPU@users.noreply.github.com>
2023-12-28 18:44:05 +0530

    Update server.js

--- university_server/server.js
+++ university_server/server.js
@@ -13,8 +13,9 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    // origin: "https://renter-khaki.vercel.app", //this is for deployed version
-    origin: "http://127.0.0.1:5500", // this is for local development
+   
+     origin: "https://xenostack.vercel.app",
+    // origin: "http://127.0.0.1:5500", // this is for local development
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
